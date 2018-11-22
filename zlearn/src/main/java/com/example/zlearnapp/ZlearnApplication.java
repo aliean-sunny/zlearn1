0463e2be5363739e62061e0f626b20648168f02d
sunny <sunny.k@zestech.in>
2018-11-22 13:45:25 +0530

    Initial Project Structure created with git and sts plugins

---
+++ zlearn/src/main/java/com/example/zlearnapp/ZlearnApplication.java
@@ -0,0 +1,12 @@
+package com.example.zlearnapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZlearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZlearnApplication.class, args);
+	}
+}
